3b1234a564c5fae7f8d6222bd6a09d705a90904a
Elena Oat <oat.elena@gmail.com>
2013-07-16 15:52:03 +0300

    add challenge questions

---
+++ challenge.py
@@ -0,0 +1,34 @@
+######################Challenge 1####################
+
+# list of women scientists 
+women_scientists = ['Anita Borg', 'Marie Curie', 'Ada Lovelace', 'Grace Hopper']
+ 
+# list of awesome women 
+awesome_women = women_scientists
+ 
+# add a scientist to list 
+awesome_women.append('Evi Nemeth')
+ 
+ 
+#1. How do the lists look like now? 
+
+
+######################Challenge 2####################
+
+# a list with cute animals
+cute_animals = ['coala', 'panda']
+# the number of the cute animals
+nr = len(cute_animals)
+ 
+# function that adds a polar bear to the cute animals
+def zoo(animals, nr_animals):
+    animals.append('polar bear')
+    nr_animals += 1 
+ 
+#1. Call zoo(cute_animals, nr) twice
+#2. How many cute animals are there? What is the nr?
+
+
+
+
+
