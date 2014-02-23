991b0921a71d2f9bcdfdb8b1335db1568723af66
elena <elena@lenochka.(none)>
2014-02-23 23:35:55 +0200

    Add 'Points to take' summary for Unicode/Encoding.

--- unicode_basics_ex3.py
+++ unicode_basics_ex3.py
@@ -1,4 +1,5 @@
 #-*- coding: utf-8-*-
+import sys
 import codecs
 #writing to a file requires bytes
 with open ('test', 'w') as f:
@@ -10,7 +11,7 @@ with open('test', 'r') as f:
         res = f.readline()
         print 'type:', type(res)
         unicode_res = res.decode('utf-8')
-        print unicode_res.encode('utf-8'), type(unicode_res)
+        print unicode_res.encode(sys.stdout.encoding), type(unicode_res)
 
 #writing to a file without encoding it
 #error, because print tries to encode into 'ascii'
