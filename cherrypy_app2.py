0df03ecd82c642e760482d53c20dd6accdf78193
Elena Oat <oat.elena@gmail.com>
2014-03-27 10:06:10 +0200

    add comments to the second file

--- cherrypy_app2.py
+++ cherrypy_app2.py
@@ -2,23 +2,27 @@ import cherrypy
 import webcolors
 
 class ColorsAndShapes:
+    #index page
     @cherrypy.expose
     def index(self):
         return "<html><body><h1>Welcome to our Shape&Color game!</h1>\
 	        Please modify the URL to view a shape and a color as following\
 		http://domainname:port/{circle, triangle, square}/color</body></html>"
 
+    #square page
     @cherrypy.expose
     def square(self, color="black"):
         return "<html><body><div style='background-color: " + color + ";\
                 height: 200px; width: 200px;'></div></body></html>"    
 
+    #circle page
     @cherrypy.expose
     def circle(self, color="black"):
         return "<html><body><div style='width: 200px;\
                 height: 200px; border-radius: 100px; background-color:" + color +  ";\
                 float: left;'></div></body></html>"
 
+    #triangle page
     @cherrypy.expose
     def triangle(self, color="black"):
         return "<html><body><div style='width: 0px; height: 0px;\
