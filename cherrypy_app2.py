3cf040fba8724a2ac8ac35cfb9f00dd421ad3feb
Elena Oat <oat.elena@gmail.com>
2014-03-27 16:29:13 +0200

    add small fixes to cherrypy_app2.py

--- cherrypy_app2.py
+++ cherrypy_app2.py
@@ -5,34 +5,34 @@ class ColorsAndShapes:
     # index page
     @cherrypy.expose
     def index(self):
-        return ("<html><body><h1>Welcome to our Shape&amp;Color game!</h1> "
-                "Please modify the URL to view a shape and a color as "
-                "following http://domainname:port/{circle, triangle, square}"
-                "/color</body></html>")
+        return ('<html><body><h1>Welcome to our Shape&amp;Color game!</h1> '
+                'Please modify the URL to view a shape and a color as '
+                'following http://domainname:port/{circle, triangle, square}'
+                '/color</body></html>')
 
     # square page
     @cherrypy.expose
     def square(self, color="black"):
-        return ("<html><body><div style='background-color: {0};"
-                "height: 200px; width: 200px;'></div>"
-		"</body></html>").format(color)
+        return ('<html><body><div style="background-color: {};'
+                'height: 200px; width: 200px;"></div>'
+		'</body></html>').format(color)
 
     # circle page
     @cherrypy.expose
     def circle(self, color="black"):
-        return ("<html><body><div style='width: 200px;"
-                "height: 200px; border-radius: 100px;"
-                "background-color: {0}; float: left;'>"
-                "</div></body></html>").format(color)
+        return ('<html><body><div style="width: 200px;'
+                'height: 200px; border-radius: 100px;'
+                'background-color: {}; float: left;">'
+                '</div></body></html>').format(color)
 
     # triangle page
     @cherrypy.expose
     def triangle(self, color="black"):
-        return ("<html><body><div style='width: 0; height: 0;"
-                "border-style: solid; border-width: 0 100px 100px 100px;"
-                "border-color: transparent transparent {0} transparent;"
-                "float: left;'></div></body></html>").format(
+        return ('<html><body><div style="width: 0; height: 0;'
+                'border-style: solid; border-width: 0 100px 100px 100px;'
+                'border-color: transparent transparent {} transparent;'
+                'float: left;"></div></body></html>').format(
                                  webcolors.name_to_hex(color))
 
-cherrypy.config.update({"server.socket_host": '0.0.0.0', "server.socket_port": 8090})
+cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 8090})
 cherrypy.quickstart(ColorsAndShapes())
