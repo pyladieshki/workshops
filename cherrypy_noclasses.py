dd3b82f18ca3f17facdf5b143280db161bb98002
Elena Oat <oat.elena@gmail.com>
2014-04-22 15:08:19 +0300

    add example where a simple function is used as a page handler

---
+++ cherrypy_noclasses.py
@@ -0,0 +1,19 @@
+import cherrypy
+
+def trythis():
+    print args
+    print kwargs
+    return "<html><body><h1>I am a simple function</h1></body></html>"
+trythis.exposed = True
+
+class SomeClass(object):
+
+    def index(self):
+        return "<html><body>I am an index method</body></html>"
+    index.exposed = True
+
+root = SomeClass()
+root.trythis = trythis
+
+if __name__ == '__main__':
+    cherrypy.quickstart(root)	    
