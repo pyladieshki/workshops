d5c1a01c5b0098f35edb839d827f3e99250aa54d
Elena Oat <oat.elena@gmail.com>
2015-04-27 21:18:59 +0300

    add example of how to get link to a photo
    The photo is searched by tag, text and upload date.

---
+++ playing_with_flickr/get_a_photo.py
@@ -0,0 +1,19 @@
+import flickrapi
+
+api_key = 'fc9d33a60904a30be522a6f6f98365dc'
+api_secret = '55f69ea50c2eb01f'
+
+# Flickr API 
+f = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json')
+# to convert date to Unix datetime use: http://www.epochconverter.com/
+photos = f.photos_search(tags='new york', per_page=1, text='New York', min_upload_date=1427897394)
+# owner of the photo
+owner = photos['photos']['photo'][0]['owner']
+# the ID of the photo
+photo_id = photos['photos']['photo'][0]['id']
+# example of a link to a photo
+# https://www.flickr.com/photos/67832671@N00/5484008696/
+# we create link to our own photo
+url = 'https://www.flickr.com/photos/%s/%s/' % (owner, photo_id)
+# display url
+print url
